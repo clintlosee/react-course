e2102b68ec8eaa111369bfa28dde556d8e66c04f
Clint Losee <clint.losee@gmail.com>
2016-03-07 15:14:09 -0700

    Added react router to reactjsprogram and set up test routes.

--- reactjsprogram/app/index.js
+++ reactjsprogram/app/index.js
@@ -1,55 +1,74 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var USER_DATA = {
-    name: 'Clint Losee',
-    username: 'clintlosee',
-    image: 'https://avatars.githubusercontent.com/u/5192207?v=3'
-}
+// var USER_DATA = {
+//     name: 'Clint Losee',
+//     username: 'clintlosee',
+//     image: 'https://avatars.githubusercontent.com/u/5192207?v=3'
+// };
+//
+// var Link = React.createClass({
+//     changeURL: function() {
+//         window.location.replace(this.props.href);
+//     },
+//     render: function() {
+//         return (
+//             <span style={{ color: 'blue', cursor: 'pointer'}} onClick={this.changeURL}>
+//                 {this.props.children}
+//             </span>
+//         );
+//     }
+// });
+//
+// var ProfilePic = React.createClass({
+//     render: function() {
+//         return (
+//             <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+//         );
+//     }
+// });
+//
+// var ProfileLink = React.createClass({
+//     render: function() {
+//         return (
+//             <div>
+//                 <Link href={'https://www.github.com/' + this.props.username}>
+//                     {this.props.username}
+//                 </Link>
+//             </div>
+//         );
+//     }
+// });
+//
+// var ProfileName = React.createClass({
+//     render: function() {
+//         return (
+//             <div>
+//                 {this.props.name}
+//             </div>
+//         );
+//     }
+// });
 
-var ProfilePic = React.createClass({
-    render: function() {
-        return (
-            <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-        );
-    }
-});
+// var Avatar = React.createClass({
+//     render: function() {
+//         return (
+//             <div>
+//                 <ProfilePic imageUrl={this.props.user.image} />
+//                 <ProfileName name={this.props.user.name} />
+//                 <ProfileLink username={this.props.user.username} />
+//             </div>
+//         );
+//     }
+// });
 
-var ProfileLink = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <a href={'https://www.github.com/' + this.props.username}>
-                    {this.props.username}
-                </a>
-            </div>
-        );
-    }
-});
-
-var ProfileName = React.createClass({
-    render: function() {
-        return (
-            <div>
-                {this.props.name}
-            </div>
-        );
-    }
-});
-
-var Avatar = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.user.image} />
-                <ProfileName name={this.props.user.name} />
-                <ProfileLink username={this.props.user.username} />
-            </div>
-        );
-    }
-});
+// ReactDOM.render(
+//     <Avatar user={USER_DATA} />,
+//     document.getElementById('app')
+// );
 
 ReactDOM.render(
-    <Avatar user={USER_DATA} />,
+    routes,
     document.getElementById('app')
 );
