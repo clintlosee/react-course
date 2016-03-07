d8924a904de48ba01a64e4c0318308ca7301ebc6
Clint Losee <clint.losee@gmail.com>
2016-03-06 21:50:59 -0700

    Updated components in reactjsprogram

--- reactjsprogram/app/index.js
+++ reactjsprogram/app/index.js
@@ -1,17 +1,55 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+    name: 'Clint Losee',
+    username: 'clintlosee',
+    image: 'https://avatars.githubusercontent.com/u/5192207?v=3'
+}
+
+var ProfilePic = React.createClass({
+    render: function() {
+        return (
+            <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+        );
+    }
+});
+
+var ProfileLink = React.createClass({
     render: function() {
         return (
             <div>
-                <h1>Hello React</h1>
+                <a href={'https://www.github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        );
+    }
+});
+
+var ProfileName = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {this.props.name}
+            </div>
+        );
+    }
+});
+
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
             </div>
         );
     }
 });
 
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 );
