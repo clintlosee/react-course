c3c59f007e6f635c7fd0fb5eb77c55cbb514a784
Clint Losee <clint.losee@gmail.com>
2016-03-28 12:11:23 -0600

    Added battle results UI, loading component, and refactored reactjsprogram.

--- reactjsprogram/app/components/ConfirmBattle.js
+++ reactjsprogram/app/components/ConfirmBattle.js
@@ -4,6 +4,8 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
+var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function puke(object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -11,8 +13,8 @@ function puke(object) {
 
 function ConfirmBattle(props) {
   return props.isLoading === true
-    ? <p> LOADING! </p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+    ? <Loading />
+    : <MainContainer>
         <h1>Confirm Players</h1>
         <div className="col-sm-8 col-sm-offset-2">
           <UserDetailsWrapper header="Player 1">
@@ -34,7 +36,7 @@ function ConfirmBattle(props) {
             </Link>
           </div>
         </div>
-      </div>
+      </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
--- reactjsprogram/app/components/UserDetails.js
+++ reactjsprogram/app/components/UserDetails.js
@@ -19,18 +19,18 @@ function UserDetails(user) {
 }
 
 UserDetails.propTypes = {
-  score: PropTypes.number,
-  info: PropTypes.shape({
-    avatar_url: PropTypes.string.isRequired,
-    blog: PropTypes.string,
-    company: PropTypes.string,
-    followers: PropTypes.number.isRequired,
-    following: PropTypes.number.isRequired,
-    location: PropTypes.string,
-    login: PropTypes.string.isRequired,
-    name: PropTypes.string,
-    public_repos: PropTypes.number.isRequired
-  })
+  // score: PropTypes.number,
+  // info: PropTypes.shape({
+  //   avatar_url: PropTypes.string.isRequired,
+  //   blog: PropTypes.string,
+  //   company: PropTypes.string,
+  //   followers: PropTypes.number.isRequired,
+  //   following: PropTypes.number.isRequired,
+  //   location: PropTypes.string,
+  //   login: PropTypes.string.isRequired,
+  //   name: PropTypes.string,
+  //   public_repos: PropTypes.number.isRequired
+  // })
 }
 
 module.exports = UserDetails;
