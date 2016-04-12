0890188fb0be0aea0e0d8e9490dca8a9cff9be5e
Clint Losee <clint.losee@gmail.com>
2016-04-12 10:41:25 -0600

    Adding initial video list setup to react-redux.

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyCDT0KOS16H4kJm7Duza1gqVGz4zqGiUL8';
 
-// Create new component. Produces HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({videos: data}); // if videos: videos, it can just be videos
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 };
 
 // Put component into the DOM
