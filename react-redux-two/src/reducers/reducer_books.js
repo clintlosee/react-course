f237fd72ec2b467f7be4b7016c0a4d4dc367e7ad
Clint Losee <clint.losee@gmail.com>
2016-04-26 09:40:53 -0600

    Finalized intial creation of actions and action creators in react-redux-two.

--- react-redux-two/src/reducers/reducer_books.js
+++ react-redux-two/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'JavaScript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'Dark Tower' },
-    { title: 'MEAN Development' }
+    { title: 'JavaScript: The Good Parts', pages: 18 },
+    { title: 'Harry Potter', pages: 48 },
+    { title: 'Dark Tower', pages: 182 },
+    { title: 'MEAN Development', pages: 8 }
   ]
 }
