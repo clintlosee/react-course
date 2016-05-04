05e2c6343f24673e1addd82652e932789423d2f6
Clint Losee <clint.losee@gmail.com>
2016-05-04 08:07:13 -0600

    Added chart component and minor styling to redux-weather.

--- redux-weather/src/containers/weather_list.js
+++ redux-weather/src/containers/weather_list.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Chart from '../components/chart';
+
 export class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-        
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * 9/5 - 459.67);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        // const pressures = _.map(cityData.list.map(weather => weather.main.pressure), (pressure) => pressure * .02952998751);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
         return (
             <tr key={name}>
                 <td>{name}</td>
+                <td><Chart data={temps} color="red" units="F" /></td>
+                <td><Chart data={pressures} color="blue" units="hPa" /></td>
+                <td><Chart data={humidities} color="green" units="%" /></td>
             </tr>
         );
     }
@@ -18,9 +27,9 @@ export class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (F)</th>
+                        <th>Barometric Pressure (Hg)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
