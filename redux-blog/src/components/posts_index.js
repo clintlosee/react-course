edc3c695076a6383ce773c7f5564c0bcc4f0d41c
Clint Losee <clint.losee@gmail.com>
2016-05-10 08:30:26 -0600

    Added initial new post component to redux-blog and started adding redux-form.

--- redux-blog/src/components/posts_index.js
+++ redux-blog/src/components/posts_index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux'; // don't need when passing fetchPosts object to connect
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
-export default () => {
-    return (
-        <div>List of Blog Posts</div>
-    );
-};
+class PostsIndex extends Component {
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a Post
+                    </Link>
+                </div>
+                List of Blog Posts
+            </div>
+        );
+    }
+}
+
+// function mapDispatchToProps(dispatch) {
+//     return bindActionCreators({ fetchPosts }, dispatch);
+// }
+
+// export default connect(null, mapDispatchToProps)(PostsIndex); // refactor to below and remove function
+export default connect(null, { fetchPosts })(PostsIndex);
